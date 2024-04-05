50dd35ad78472d37945cdaa29f18b29aa04110c9
nathanrowell <nrowell@udel.edu>
2024-04-05 19:21:24 -0400

    trying to fix

--- src/player.ts
+++ src/player.ts
@@ -2,14 +2,16 @@ export class Player {
     firstName: string;
     lastName: string;
     skill: number;
-    image: string
-    constructor (firstName: string, lastName: string, skill: number, image: string) {
+    image: string;
+
+    constructor(firstName: string, lastName: string, skill: number, image: string) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.skill = skill;
         this.image = image;
     }
 }
+
 const player1 = new Player("Tori", "Deal", 6, "https://miro.medium.com/v2/resize:fit:800/1*ujqWWuAs8Wf5NXtx1DXQ0g.jpeg");
 const player2 = new Player("Derrick", "Kosinkski", 6, "https://miro.medium.com/v2/resize:fit:800/1*Pd2kx0bs2wYPv83VHPyR0A.jpeg");
 const player3 = new Player("Jordan", "Wiseley", 8, "https://miro.medium.com/v2/resize:fit:800/1*h1RT5ttHtAIRPaEC_XW7RQ.jpeg");
@@ -20,5 +22,5 @@ const player7 = new Player("Camila", "Nakagawa", 6, "https://static.wikia.nocook
 const player8 = new Player("Cara Maria", "Sorbello", 6, "https://miro.medium.com/v2/resize:fit:1200/1*8mnAnvGlHc3Y1X8Xv1delg.jpeg");
 const player9 = new Player("Jenna", "Compono", 6, "https://miro.medium.com/v2/resize:fit:1600/1*fSQGgp1fu9OXvGZk05ulzg.jpeg");
 const player10 = new Player("Kailah", "Casillas", 6, "https://miro.medium.com/v2/resize:fit:473/1*i5l8M3pg205GtrEmYdbZrQ.png");
-export let arr1 : Player[] = new Array();
-arr1.push(player1,player2,player3,player4,player5,player6,player7,player8,player9,player10);
+
+export let arr1: Player[] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10];
